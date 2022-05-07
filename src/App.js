13db56fcd12550f86f25a7466ea8d9d4d0c4c0b0
sohara <steph.ohara@icloud.com>
2022-05-07 10:15:08 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
-import React from 'react'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
