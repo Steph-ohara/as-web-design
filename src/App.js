9fb53cdf1b9455e5e58596e331a08792abc9b5da
sohara <steph.ohara@icloud.com>
2022-05-07 09:31:56 -0700

    cleared old code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react'
 
-import { Footer, Blog, Features, Header } from './containers';
-import { CTA, Brand, Navbar } from './components';
+import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <div class="App">
-      <div class="gradient">
-        <Navbar />
-        <Header />
-      </div>
-
-      <div>
-        <Brand />
-        <WhatGPT3 />
-        <Features />
-        <Possibility />
-        <CTA />
-        <Blog />
-        <Footer />
-      </div>
-    </div>
+    <div>App</div>
   )
 }
 
